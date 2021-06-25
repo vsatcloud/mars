33a9792512c7b1ce491d8dd1077221a38431d853
andyidea <45716108@qq.com>
2021-06-25 15:50:29 +0800

    添加头像字段

--- models/admin.go
+++ models/admin.go
@@ -7,19 +7,20 @@ import (
 
 type AdminModel struct {
 	gorm.Model
-	Username    string `gorm:"type:varchar(128)"` // 用户名
-	Password    string `gorm:"type:varchar(128)"` // 密码
-	Salt        string `gorm:"type:varchar(128)"` // 盐
-	RealName    string `gorm:"type:varchar(128)"` // 真实姓名ar
-	AuthorityID string `gorm:"type:varchar(512)"` // 角色
+	Username string `gorm:"type:varchar(128)"` // 用户名
+	Password string `gorm:"type:varchar(128)"` // 密码
+	Salt     string `gorm:"type:varchar(128)"` // 盐
+	RealName string `gorm:"type:varchar(128)"` // 真实姓名ar
+	Roles    string `gorm:"type:varchar(512)"` // 角色
+	Avatar   string //头像
 }
 
 func (am *AdminModel) GenToken(secret string) (string, error) {
 	var data = map[string]interface{}{
-		"user_id":      am.ID,
-		"username":     am.Username,
-		"authority_id": am.AuthorityID,
-		"created_at":   am.CreatedAt.Unix(),
+		"user_id":    am.ID,
+		"username":   am.Username,
+		"roles":      am.Roles,
+		"created_at": am.CreatedAt.Unix(),
 	}
 
 	return utils.GenerateToken(data, secret)
