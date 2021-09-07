e8dfb277b2dc5817cdc62f339e8f12e3b26578b5
andyidea <45716108@qq.com>
2021-09-07 15:16:43 +0800

    分页显示优化

--- proto/proto.go
+++ proto/proto.go
@@ -2,9 +2,12 @@ package proto
 
 //Pagination 分页
 type Pagination struct {
-	Page  int `form:"page" default:"1" json:"page"`
+	//页码 从1开始
+	Page int `form:"page" default:"1" json:"page"`
+	//兼容字段-勿用，请使用size字段
 	Limit int `form:"limit" default:"10" json:"limit"` //逐步弃用
-	Size  int `form:"size" default:"10" json:"size"`
+	//每页的数量
+	Size int `form:"size" default:"10" json:"size"`
 }
 
 func (p *Pagination) Offset() int {
